1a0b3fab59a9972ac49644d64346793333c7c646
Max Dyachuk <dyachukmv1@gmail.com>
2019-11-19 12:39:16 +0200

    Update Department.java

--- src/com/company/Department.java
+++ src/com/company/Department.java
@@ -5,22 +5,27 @@ import java.util.List;
 
 class Department {
 
-    public static void main(String[] args) {
-        List<Employee> managers = new ArrayList<>();
+    private List<Employee> managers = new ArrayList<>();
 
-        Manager manager1 = new Manager("Ada", "Petersen", 2500, 4);
-        manager1.addTeamMember(new Developer("Quintin", "Pennington", 4500, 7, manager1));
-        manager1.addTeamMember(new Developer("Duncan", "Clarke", 3500, 5, manager1));
-        manager1.addTeamMember(new Developer("Ayanna", "Wood", 3500, 5, manager1));
-        manager1.addTeamMember(new Developer("Tim", "Lowell", 600, 0.5, manager1));
-        manager1.addTeamMember(new Designer("Myla", "Bowman", 3000, 4, 1, manager1));
-        manager1.addTeamMember(new Designer("Angelique", "Molina", 1500, 1.7, 0.8, manager1));
-        managers.add(manager1);
+    Department() {}
 
-        Manager manager2 = new Manager("Amelia", "Heath", 1500, 2);
-        manager2.addTeamMember(new Developer("Alex", "Smith", 500, 0, manager2));
-        managers.add(manager2);
+    void addManager(Employee employee) {
+        if (employee instanceof Manager) {
+            managers.add(employee);
+        } else {
+            System.out.println("Can not add employee to department.");
+        }
+    }
 
+    void removeManager(Employee employee) {
+        managers.remove(employee);
+    }
+
+    Employee getManager(int i) {
+        return managers.get(i);
+    }
+
+    void giveSalary() {
         managers.forEach(Employee::giveSalary);
     }
 }
